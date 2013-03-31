08b67b1d0bb8b1302f07da674cfec7c830e4e400
Luiz Fonseca <runeroniek@gmail.com>
2013-03-31 18:11:58 -0300

    Setup FAQ relationship and add tags method
    - You can now list all tags from a given FAQ

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,3 +1,10 @@
 class Faq < ActiveRecord::Base
-  belongs_to :user_id
+  belongs_to :user
+  has_many :questions
+
+
+
+  def tags
+    self.questions.pluck('distinct(unnest(tags))')    
+  end
 end
