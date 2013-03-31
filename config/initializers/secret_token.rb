0a0e6dc6aac0b46412d85e938f8581851bc69371
Luiz Fonseca <runeroniek@gmail.com>
2013-03-31 11:31:43 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faqly::Application.config.secret_key_base = '08795b971c0ce899c73228a26f107f42b487c31294530f7668e5c7c4561ac8ddca63d36957b9b97708d9c343a9355aa014d90d7c72a05b178894b21918cd7bdd'
