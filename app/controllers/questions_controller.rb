b0af8705c026868ad3df23acb505477094d1553b
Luiz Fonseca <runeroniek@gmail.com>
2013-03-31 21:33:13 -0300

    Order questions by creation date.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
 
   def index
     if params[:tag] && params[:tag].present?
-      @questions = Question.where('? = ANY(tags)', params[:tag].to_s)
+      @questions = Question.where('? = ANY(tags)', params[:tag].to_s).order('created_at ASC')
     end
     index!
   end
