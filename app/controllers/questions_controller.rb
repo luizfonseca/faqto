f68eceb2cb0643ad86e5090bed2a01f70f744485
Luiz Fonseca <eu@luiz.cc>
2015-08-09 03:12:38 -0300

    Fix weird behavior

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,11 @@
 class QuestionsController < ApplicationController
 
   def index
+    @faq = Faq.find_by id: params[:faq_id]
     if params[:tag] && params[:tag].present?
       @questions = Question.where('? = ANY(tags)', params[:tag].to_s).order('created_at ASC')
     end
+    
   end
 
 end
