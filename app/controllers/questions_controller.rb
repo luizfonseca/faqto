46a63d7c68c4876d0db505451c72c457a2e05afa
Luiz Fonseca <eu@luiz.cc>
2015-08-09 03:07:12 -0300

    Fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,6 @@ class QuestionsController < ApplicationController
     if params[:tag] && params[:tag].present?
       @questions = Question.where('? = ANY(tags)', params[:tag].to_s).order('created_at ASC')
     end
-    index!
   end
 
 end
